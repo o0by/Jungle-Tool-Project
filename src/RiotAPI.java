1be3e183dd3fba0c58e55972cd7b150284da6c43
o0by <luc.t.le2007@gmail.com>
2025-12-03 23:37:49 -0500

    Riot API class

--- src/RiotAPI.java
+++ src/RiotAPI.java
@@ -1,5 +1,29 @@
 package src;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class RiotAPI {
+    private final String apiKey;
+    private final HttpClient client; 
+    
+    public RiotAPI(String apiKey){
+        this.apiKey = apiKey;
+        this.client = HttpClient.newHttpClient();
+    }
+
+    public String get(String url) throws Exception{
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).header("X-Riot-Token", apiKey).build();
+
+        HttpResponse <String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
+
+    }
+
+
+
     
 }
