60d546971d52daee2ee9082a99746a458d16a453
o0by <luc.t.le2007@gmail.com>
2025-12-07 03:35:48 -0500

    RiotAPI comments

--- src/RiotAPI.java
+++ src/RiotAPI.java
@@ -14,9 +14,18 @@ public class RiotAPI {
         this.client = HttpClient.newHttpClient();
     }
 
+    /*  Taking url as input
+        returning string (raw JSON data from riot server)
+     */    
     public String get(String url) throws Exception{
+        //Building HTTP GET request, turning url into URI object, required API key in X-Riot-Token, then finalizing request
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).header("X-Riot-Token", apiKey).build();
 
+        /*
+        **Sending Request to client
+        **response holds status code and the JSON body that riot will return 
+        **then converts response into normal string
+        */
         HttpResponse <String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         return response.body();
